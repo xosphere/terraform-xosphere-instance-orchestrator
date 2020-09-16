979e02e16df31191a40b68d1be99a6215415cd01
changsu <changsu@xosphere.io>
2020-09-16 09:59:41 -0700

    typo fixed

--- main.tf
+++ main.tf
@@ -424,7 +424,7 @@ resource "aws_lambda_function" "xosphere_instance_orchestrator_lambda_k8s_enable
 }
 
 resource "aws_lambda_function_event_invoke_config" "xosphere_instance_orchestrator_lambda_k8s_enabled_invoke_config" {
-  count = length(var.k8s_vpc_security_group_ids) == 0  || length(var.k8s_vpc_subnet_ids) == 0 ? 1 : 0
+  count = length(var.k8s_vpc_security_group_ids) > 0  || length(var.k8s_vpc_subnet_ids) > 0 ? 1 : 0
   function_name = aws_lambda_function.xosphere_instance_orchestrator_lambda_k8s_enabled[count.index].function_name
   maximum_retry_attempts = 0
   qualifier = "$LATEST"
